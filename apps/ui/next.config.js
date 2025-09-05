1fa27ce3ecb6018996beafe00d56bb4d319ccd07
Tim Siebels <tim@eduplaces.de>
2025-09-05 13:56:11 +0200

    Dont run eslint during next build

--- apps/ui/next.config.js
+++ apps/ui/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // We run this separately
+  },
+};
 
 export default nextConfig;
